df84f57aa79265708a129e2a63b3e15fe6297b52
Saurabh <saurabh.av@cureondelivery.com>
2017-04-15 20:33:32 +0530

    adding tests

--- coinsTask/transactions/tests.py
+++ coinsTask/transactions/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PaymentsTestCase(APITestCase):
+    url = "/v1/payments/"
+    def test_payments_get_api(self):        
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
